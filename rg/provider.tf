388f0f09d627b317852b32f730da8af931063d06
Sujeet <raisujeet209@gmail.com>
2024-07-10 11:14:02 +0530

    backend create

--- rg/provider.tf
+++ rg/provider.tf
@@ -4,8 +4,17 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.98.0"
     }
+    
+  }
+  
+  backend "azurerm" {
+    resource_group_name  = "sujeet-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "rohitstg"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "rohitcon"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
+
 
 provider "azurerm" {
 features {}
